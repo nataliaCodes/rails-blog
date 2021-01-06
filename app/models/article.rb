6207d71c46a8a975d6cdb54f720af753f7f2238e
Natalia Martian <natalia.martian@gmail.com>
2021-01-06 04:34:40 +0000

    Update article Model to include destroy dependency for comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
-  has_many :comments
+  #an article can have many comment
+  #if an article is deleted, its dependents(comments) will also be destroyed
+  has_many :comments, dependent: destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
